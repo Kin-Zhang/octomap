9279b2db0512f73c2faa880a63ec947ad1334c52
Kin <kinzhangglimmer@gmail.com>
2023-04-05 23:37:39 +0200

    feat: delete files we don't need TODO finish the script... hopefully it's easy compared with ERASOR.

---
+++ octomap_PCD.cpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2022-now, RPL, KTH Royal Institute of Technology
+ * MIT License
+ * @author Kin ZHANG (https://kin-zhang.github.io/)
+ * @date: 2023-04-05 23:28
+ * @details: No ROS version, speed up the process
+ *
+ * Input: PCD files + Prior raw global map , check our benchmark in dufomap
+ * Output: Cleaned global map
+ */
+
